0397362d7377ff129240dc18af0a99576ae0381c
Your Name <you@example.com>
2022-06-18 12:32:44 -0300

    add metodo post

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -11,6 +11,25 @@ async function get(req, res) {
     })
 }
 
+
+async function post(req, res) {
+    const {
+        name,
+        brand,
+        price,
+    
+    } = req.body
+
+    const product = new ProductsModel ({
+        name,
+        brand,
+        price
+    })
+    product.save()
+    res.send()
+}
+
 module.exports = {
     get,
+    post,
 }
--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ db.connect()
 
 
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.use('/api', routes)
 
